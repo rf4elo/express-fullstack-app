805ffe817db8afe00188028b1e5e4ea338cdf645
Rafael <rafaelrocha57852@gmail.com>
2026-07-04 22:43:09 -0300

    Front-end routes added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import e from "express";
 import path from "path";
-import productRoutes from "./routes/productRoutes.js";
-import usersRoutes from "./routes/userRoutes.js";
 import { fileURLToPath } from "url";
 
+import productRoutes from "./routes/productRoutes.js";
+import usersRoutes from "./routes/userRoutes.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,7 +15,7 @@ const app = e();
 // FRONT END SETTINGS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
-app.use(e.static(path.join('../', 'public')));
+app.use(e.static(path.join(__dirname, '../public')));
 
 
 // BACKEND MIDDLEWARES
@@ -33,7 +33,44 @@ app.use('/api', usersRoutes);
 
 // '/' GET ROUTE
 app.get('/', (req, res) => {
-    res.status(200).json({ "message":"This app is backend" });
+    
+    res.render('../views/index.ejs')
+
+});
+
+app.get('/users', async (req, res) => {
+
+    const response = await fetch("http://localhost:80/api/users", {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const users = await response.json();
+
+    res.render('../views/pages/users.ejs', users);
+
+});
+
+app.get('/products', async (req, res) => {
+
+    const response = await fetch('http://localhost/api/products', {
+        method: "GET",
+        headers: {
+            'Content-Type':'application/json'
+        }
+    });
+
+    const products = await response.json();
+
+    const pageData = {
+        "title": "Products",
+        "products": products,
+    };
+
+    res.render('../views/pages/products.ejs', pageData);
+
 });
 
 export default app;
